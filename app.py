95b437c1f7e4dfb0b82ffc828c764456d487bb4a
ashwin ram <48376310+AshwinRam2000@users.noreply.github.com>
2019-09-20 17:18:00 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, url_for, Response
 from PIL import Image
 from matplotlib import pyplot as plt
 import numpy as np
-import face_recognition
 import keras
 from keras.models import load_model
 import cv2
@@ -12,24 +11,24 @@ app = Flask(__name__)
 
 
 
-def predict(image):
-    print(image)
-    image = face_recognition.load_image_file(image)
-    face_locations = face_recognition.face_locations(image)
-    top, right, bottom, left = face_locations[0]
-    face_image = image[top:bottom, left:right]
-    face_image = cv2.resize(face_image, (48,48))
-    face_image = cv2.cvtColor(face_image, cv2.COLOR_BGR2GRAY)
-    face_image = np.reshape(face_image, [1, face_image.shape[0], face_image.shape[1], 1])
-    emotion_dict= {'Angry': 0, 'Sad': 5, 'Neutral': 4, 'Disgust': 1, 'Surprise': 6, 'Fear': 2, 'Happy': 3}
-    model = load_model("model_v6_23.hdf5")
-    predicted_class = np.argmax(model.predict(face_image))
-    label_map = dict((v,k) for k,v in emotion_dict.items()) 
-    predicted_label = label_map[predicted_class]
+# def predict(image):
+#     print(image)
+#     image = face_recognition.load_image_file(image)
+#     face_locations = face_recognition.face_locations(image)
+#     top, right, bottom, left = face_locations[0]
+#     face_image = image[top:bottom, left:right]
+#     face_image = cv2.resize(face_image, (48,48))
+#     face_image = cv2.cvtColor(face_image, cv2.COLOR_BGR2GRAY)
+#     face_image = np.reshape(face_image, [1, face_image.shape[0], face_image.shape[1], 1])
+#     emotion_dict= {'Angry': 0, 'Sad': 5, 'Neutral': 4, 'Disgust': 1, 'Surprise': 6, 'Fear': 2, 'Happy': 3}
+#     model = load_model("model_v6_23.hdf5")
+#     predicted_class = np.argmax(model.predict(face_image))
+#     label_map = dict((v,k) for k,v in emotion_dict.items()) 
+#     predicted_label = label_map[predicted_class]
 
 
-    print(predicted_label)
-    return predicted_label
+#     print(predicted_label)
+#     return predicted_label
 
 
 def sentiment_scores(sentence): 
@@ -85,12 +84,12 @@ def hello_name(name):
                 sen = request.form.get('sentence')
                 print("++++++++++++++++",sen)
                 score,neg,pos=sentiment_scores(sen)
-        if name=="pic":
-                file=request.files['image']
-                print(file)
+        # if name=="pic":
+        #         file=request.files['image']
+        #         print(file)
                 
         
-                status=predict(file)
+        #         status=predict(file)
         
 
         
